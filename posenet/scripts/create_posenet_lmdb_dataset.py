95131c08a95f6c476abf4cf9c4adb7dfdba018c7
jaehyun <lim0505@gmail.com>
2016-02-15 14:00:10 +0900

    minor changes in create_posenet_lmdb_dataset.py

--- posenet/scripts/create_posenet_lmdb_dataset.py
+++ posenet/scripts/create_posenet_lmdb_dataset.py
@@ -1,4 +1,4 @@
-caffe_root = '.../caffe-posenet/'  # Change to your directory to caffe-posenet
+caffe_root = '/home/jaehyun/github/caffe-posenet-alexgkendall/'  # Change to your directory to caffe-posenet
 import sys
 sys.path.insert(0, caffe_root + 'python')
 
@@ -10,48 +10,60 @@ import random
 from scipy import misc
 import random
 
-directory = '.../CambridgeLandmarks/Kings/'
-dataset = 'dataset_train.txt'
+data_root = '/media/data1/image/cambridge_landmarks'
+#data_prefix='KingsCollege'
+data_prefices = ['KingsCollege', 'GreatCourt', 'OldHospital', 'ShopFacade', 'StMarysChurch', 'Street']
+#data_split='test' #'train'
+data_splits=['train', 'test']
 
-poses = []
-images = []
+for data_prefix in data_prefices:
+    for data_split in data_splits:
+        print 'data_root: {}'.format(data_root) 
+        print 'data_prefix: {}'.format(data_prefix)
+        print 'data_split: {}'.format(data_split)
 
-with open(directory+dataset) as f:
-    next(f)  # skip the 3 header lines
-    next(f)
-    next(f)
-    for line in f:
-        fname, p0,p1,p2,p3,p4,p5,p6 = line.split()
-        p0 = float(p0)
-        p1 = float(p1)
-        p2 = float(p2)
-        p3 = float(p3)
-        p4 = float(p4)
-        p5 = float(p5)
-        p6 = float(p6)
-        poses.append((p0,p1,p2,p3,p4,p5,p6))
-        images.append(directory+fname)
-
-r = list(range(len(images)))
-random.shuffle(r)
-
-print 'Creating PoseNet Dataset.'
-env = lmdb.open('posenet_dataset_lmdb', map_size=int(1e12))
-
-count = 0
-
-for i in r:
-    if (count+1) % 100 == 0:
-        print 'Saving image: ', count+1
-    X = misc.imread(images[i])
-    X = misc.imresize(X, (341,256))
-    X = np.transpose(X,(2,0,1))
-    im_dat = caffe.io.array_to_datum(np.array(X).astype(np.uint8))
-    im_dat.float_data.extend(poses[i])
-    str_id = '{:0>10d}'.format(count)
-    with env.begin(write=True) as txn:
-        txn.put(str_id, im_dat.SerializeToString())
-    count = count+1
-
-env.close()
+        directory = '{}/{}/'.format(data_root, data_prefix)
+        dataset = 'dataset_{}.txt'.format(data_split)
+        
+        poses = []
+        images = []
+        
+        with open(directory+dataset) as f:
+            next(f)  # skip the 3 header lines
+            next(f)
+            next(f)
+            for line in f:
+                fname, p0,p1,p2,p3,p4,p5,p6 = line.split()
+                p0 = float(p0)
+                p1 = float(p1)
+                p2 = float(p2)
+                p3 = float(p3)
+                p4 = float(p4)
+                p5 = float(p5)
+                p6 = float(p6)
+                poses.append((p0,p1,p2,p3,p4,p5,p6))
+                images.append(directory+fname)
+        
+        r = list(range(len(images)))
+        random.shuffle(r)
+        
+        print 'Creating PoseNet Dataset.'
+        env = lmdb.open('{}/{}_{}_lmdb'.format(data_root, data_prefix, data_split), map_size=int(1e12))
+        
+        count = 0
+        
+        for i in r:
+            if (count+1) % 100 == 0:
+                print 'Saving image: ', count+1
+            X = misc.imread(images[i])
+            X = misc.imresize(X, (341,256), interp='bicubic')
+            X = np.transpose(X,(2,0,1))
+            im_dat = caffe.io.array_to_datum(np.array(X).astype(np.uint8))
+            im_dat.float_data.extend(poses[i])
+            str_id = '{:0>10d}'.format(count)
+            with env.begin(write=True) as txn:
+                txn.put(str_id, im_dat.SerializeToString())
+            count = count+1
+        
+        env.close()
 
